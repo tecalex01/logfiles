bac8322a3a57e283a1423929cee33bdcf27175b9
alexdel <alexdel@ALEXDEL-LAP.oradev.oraclecorp.com>
2021-04-26 13:08:33 -0500

    This commit makes following changes. 1. pom.xml    1.1. Add following dependencies:         - dropwizard: To get a set of libraries for:             Jetty   http server             Jersey  RESTful web framework             Jackson JSON libray         - JUnit : For testing 	        - JUnit-addons: For comparing files test
       1.2. Add following plugins:
            - maven-surefire-plugin: For integration with test
            - maven-javadoc-plugin: To generate documentation
            - maven-compiler-plugin: To specify java compiler
            - maven-shade-plugin: To package artifact in .jar file
    
    2. Basic changes on LogFileApp.java and LogFileConf to start running
    Restfull api
    
    3. Add config.yml. Place where configuration occurs to drop wizard, here
    we can define variables, connection to database, etcetera.
    
    4. Documentation.
       - logfile.yaml swagger Restfull api definition for this logfile api.
       - backend.jpg: Class diagram for backend classes.

--- src/main/java/com/logfiles/LogFilesApp.java
+++ src/main/java/com/logfiles/LogFilesApp.java
@@ -1,5 +1,21 @@
 package com.logfiles;
 
-public class LogFilesApp {
+import io.dropwizard.Application;
+import io.dropwizard.setup.Environment;
 
+public class LogFilesApp extends Application<LogFilesConf> {
+	
+	public static void main(String []args) throws Exception
+	{
+		new LogFilesApp().run(new String[] {"server", "config.yml"});
+	}
+
+	@Override
+	public void run(LogFilesConf configuration, Environment environment)
+			throws Exception {
+
+		
+	}
+
+	
 }
--- src/main/java/com/logfiles/LogFilesConf.java
+++ src/main/java/com/logfiles/LogFilesConf.java
@@ -1,5 +1,25 @@
 package com.logfiles;
 
-public class LogFilesConf {
+import javax.validation.constraints.NotEmpty;
 
+import io.dropwizard.Configuration;
+
+public class LogFilesConf extends Configuration {
+	@NotEmpty
+	private String defaultHost;
+	@NotEmpty
+	private String logDirectory;
+	
+	public String getDefaultHost() {
+		return defaultHost;
+	}
+	public void setDefaultHost(String defaultHost) {
+		this.defaultHost = defaultHost;
+	}
+	public String getLogDirectory() {
+		return logDirectory;
+	}
+	public void setLogDirectory(String logDirectory) {
+		this.logDirectory = logDirectory;
+	}	
 }
